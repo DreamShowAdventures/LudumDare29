8ca4a528f3d33963c02761682d07456af83b8a4c
steverichey <stevenpatrickrichey@gmail.com>
2014-04-25 13:52:58 -0500

    preparing build process for LD

--- GruntFile.js
+++ GruntFile.js
@@ -3,6 +3,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -16,10 +18,10 @@ module.exports = function (grunt) {
         },
         concat: {
             dist: {
-                src: [  "src/lib/**/*.js",
-                    "src/game/**/*.js"
+                src: [  "src/lib/phaser.js",
+						"src/game/*.js"
                      ],
-                dest: 'deploy/js/<%= pkg.name %>.js'
+                dest: 'src/bin/<%= pkg.name %>.js'
             }
         },
         watch: {
@@ -30,10 +32,30 @@ module.exports = function (grunt) {
             dev: {
                 path: 'http://localhost:8080/index.html'
             }
-        }
+        },
+		uglify: {
+			options: {
+				compress: {
+					drop_console: true
+				}
+			},
+			my_target: {
+				files: {
+					'deploy/js/<%= pkg.name %>.min.js' : ['src/bin/<%= pkg.name %>.js']
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{expand: true, src: ['assets/*'], dest: 'deploy/'},
+					{expand: true, flatten: true, src: ['src/index.html'], dest: 'deploy/'}
+				]
+			}
+		}
     });
 
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify', 'copy']);
     grunt.registerTask('open-watch', ['concat', 'connect', 'open', 'watch']);
 
 }
--- build.js
+++
@@ -1,3 +0,0 @@
-var fs = require('fs');
-
-fs.createReadStream('test.log').pipe(fs.createWriteStream('out/newLog.log'));
