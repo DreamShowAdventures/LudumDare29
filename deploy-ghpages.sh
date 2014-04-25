704188207fc9706be00c9bc7b632f77ed9efe144
steverichey <stevenpatrickrichey@gmail.com>
2014-04-25 10:05:57 -0500

    fix unterminated string, update git script

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -2,10 +2,14 @@
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/DreamShowAdventures/LudumDare29 gh-pages > /dev/null
+
+# Build to deploy folder
+grunt
 
 # Commit, push changes
-cd gh-pages
+cd deploy
+git init
+git remote add origin https://github.com/DreamShowAdventures/LudumDare29.git
 git add .
-git commit -m "Build successful, build $TRAVIS_BUILD_NUMBER pushed to gh-pages
+git commit -m "Build successful, build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
