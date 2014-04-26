08fa236ac75ab8cb8e1323fe6e0dad067a8e558d
steverichey <stevenpatrickrichey@gmail.com>
2014-04-26 10:19:37 -0500

    add bunny reference inside bunny class BUNCEPTION

--- src/game/Bunny.js
+++ src/game/Bunny.js
@@ -5,28 +5,25 @@
  * @author Steve Richey http://www.steverichey.com @stvr_tweets
  */
 
-var cursors;
-
 Bunny = function (game) {
 	var x = 10;
 	var y = 10;
 	
 	game.physics.enable(this, Phaser.Physics.Arcade);
-	game.add.sprite(10, 10, 'bunny')
+	this.bunny = game.add.sprite(10, 10, 'bunny')
 	
-	cursors = game.input.keyboard.createCursorKeys();
+	this.cursors = game.input.keyboard.createCursorKeys();
 	//jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-	this.update.bind(this);
 };
 
 Bunny.prototype.update = function() {
-	this.body.velocity.x = 0;
+	this.bunny.body.velocity.x = 0;
 	
-	if (cursors.left.isDown) {
-		this.body.velocity.x = -50;
+	if (this.cursors.left.isDown) {
+		this.bunny.body.velocity.x = -50;
 	}
 	
-	if (cursors.right.isDown) {
-		this.body.velocity.x = 50;
+	if (this.cursors.right.isDown) {
+		this.bunny.body.velocity.x = 50;
 	}
 };
