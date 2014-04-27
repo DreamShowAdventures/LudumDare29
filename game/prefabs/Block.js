41277afd2c32e4ff824ccc64d861bbb79ed8eed8
steverichey <stevenpatrickrichey@gmail.com>
2014-04-27 13:31:53 -0500

    refresh travis

--- game/prefabs/Block.js
+++ game/prefabs/Block.js
@@ -3,7 +3,7 @@
 var Block = function(game, x, y, frame) {
 	Phaser.Sprite.call(this, game, x, y, 'dirt', frame);
 	this.frame = game.rnd.integerInRange(0, 14);//
-	// scale up!
+	// scale up! //
 	this.smoothed = false;
 	this.scale.x = 2;
 	this.scale.y = 2;
